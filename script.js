d9f2cbd6606f0acf8642f141ef1bfdd138511297
Yellow Gold <Ashyraffa>
2025-09-12 20:55:57 +0700

    revamped website

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.questions').forEach(button => {
-    button.addEventListener('click', () => {
-        const answers = button.nextElementSibling;
-        answers.style.display = answers.style.display === 'block' ? 'none' : 'block';
-    });
-});
